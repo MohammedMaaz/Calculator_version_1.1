b240c40a431a030a110b34820033ace67d4a179b
MohammedMaaz <maazproductions25@gmail.com>
2017-03-23 02:54:22 +0500

    commit2

--- headers/customButtons.h
+++ headers/customButtons.h
@@ -6,8 +6,14 @@
     Button b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, bDot, bPlus, bMinus, bDivide, bMultiply, bPercent, bSq, bCube,bNpow, bFact,bLeftB,bRightB,bReciprocal,bSin,bCos, bTan, bASin, bACos, bATan,
     bSqrt, bCbRt, bNroot, bLog, bLn, bExp, b10x, bPie,bEquals,bBackspace,bClear; //global objects
 
-    vector<string> buttonStrings = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "+", "-", "/", "*", "%", "x2", "x3", "^", "!", "(", ")", "x-1", "sin", "cos", "tan", "sin-1", "cos-1", "tan-1","sqrt", "cbrt",
-                                        "log(", "ln(", "e^(", "10^(", "pi","=", "", ""}
+    vector<string> buttonStrings = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "+", "-", "/", "*", "%", "x2", "x3", "^", "!", "(", ")", "x-1", "sin", "cos", "tan", "sin-1", "cos-1", "tan-1","sqrt", "cbrt", "nthrt"
+                                        "log", "ln", "e", "*10^", "pi","=", "BSP", "C"};
+
+    vector<string> showString = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "+", "-", "/", "*", "%", "^2", "^3", "^", "!", "(", ")", "^(-1)", "sin", "cos", "tan", "asin", "acos", "atan","^(1/2)", "^(1/3)", "^(1/"
+                                        "log", "ln", "e^", "*10^", "pi","", "", ""};
+
+    vector<string> returnString = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "+", "-", "/", "x", "%", "^2", "^3", "^", "!", "(", ")", "1/(", "s(", "c(", "t(", "S(", "C(", "T(","q(", "w(",
+                                        "L(", "l(", "e^(", "10^(", "p","=", "", ""}
 
     sf::Font font;
     loadFile("fonts/DroidSansMono.ttf",font);
